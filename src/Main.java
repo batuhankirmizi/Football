e7a6b3478f39c61c9bd35a65a4916f78e15f9b70
robobok <robobok@users.noreply.github.com>
2016-12-10 16:31:34 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -29,12 +29,62 @@ public class Main extends JPanel implements KeyListener {
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-
+				System.out.println(players[0].j+" "+players[0].ySpeed);
+				if(players[0].i>0)
+					players[0].xSpeed=players[0].speedM*Math.log(Math.abs(players[0].i));
+				else if (players[0].i<0) players[0].xSpeed=-players[0].speedM*Math.log(Math.abs(players[0].i));
+				else players[0].xSpeed=0;
+				
+				if(players[0].j>0)
+				players[0].ySpeed=players[0].speedM*Math.log(Math.abs(players[0].j));
+				else if(players[0].j<0)players[0].ySpeed=-players[0].speedM*Math.log(Math.abs(players[0].j));
+				else players[0].ySpeed=0;
+				
+				
+				if(players[0].getCenterY()<=0)										//stop players from moving out of frame
+				players[0].setCenterY(players[0].getCenterY() + players[0].movspeed);
+				if(players[0].getCenterY()>=HEIGHT - 70)
+				players[0].setCenterY(players[0].getCenterY() - players[0].movspeed);
+				if(players[0].getCenterX()<=0)
+				players[0].setCenterX(players[0].getCenterX() + players[0].movspeed);
+				if(players[0].getCenterX() >= WIDTH - 40)
+				players[0].setCenterX(players[0].getCenterX() - players[0].movspeed);
+                        
+				
+				players[0].setCenterY(players[0].getCenterY() + players[0].ySpeed);
+				players[0].setCenterX(players[0].getCenterX() + players[0].xSpeed);
+				
+				if(pressed.contains(KeyEvent.VK_W) && !pressed.contains(KeyEvent.VK_S))
+					if(players[0].j>-120)players[0].j-=4;
+					else players[0].j=-125;
+				else if(!pressed.contains(KeyEvent.VK_W) && pressed.contains(KeyEvent.VK_S))
+					if(players[0].j<120)players[0].j+=4;
+					else players[0].j=125;
+				else{
+					if(players[0].j>6)players[0].j-=6;
+					else if(players[0].j<-6)players[0].j+=6;
+					else players[0].j=0;
+				}
+				
+				if(pressed.contains(KeyEvent.VK_A) && !pressed.contains(KeyEvent.VK_D))
+					if(players[0].i>-120)players[0].i-=4;
+					else players[0].i=-125;
+				else if(!pressed.contains(KeyEvent.VK_A) && pressed.contains(KeyEvent.VK_D))
+					if(players[0].i<120)players[0].i+=4;
+					else players[0].i=125;
+				else{
+					if(players[0].i>6)players[0].i-=6;
+					else if(players[0].i<-6)players[0].i+=6;
+					else players[0].i=0;
+				}
+				
+				
+				/*
                 if(pressed.contains(KeyEvent.VK_W) && !pressed.contains(KeyEvent.VK_S)) {
                     if(players[0].getCenterY() >= 0)
                         players[0].setCenterY(players[0].getCenterY() - players[0].movspeed);
                     players[0].xSpeed=-players[0].movspeed;
-                } else if(!pressed.contains(KeyEvent.W) && pressed.contains(KeyEvent.VK_S)) {
+                } else if(!pressed.contains(KeyEvent.VK_W) && pressed.contains(KeyEvent.VK_S)) {
                     if(players[0].getCenterY() <= HEIGHT - 70)
                         players[0].setCenterY(players[0].getCenterY() + players[0].movspeed);
                     players[0].xSpeed=players[0].movspeed;
@@ -50,9 +100,10 @@ public class Main extends JPanel implements KeyListener {
                     players[0].ySpeed=players[0].movspeed;
                 } else players[0].ySpeed=0;
 				
-				if(((pressed.contains(KeyEvent.VK_LEFT) && !pressed.contains(KeyEvent.VK_RIGHT))||(!pressed.contains(KeyEvent.VK_LEFT) && pressed.contains(KeyEvent.VK_RIGHT)))&&((pressed.contains(KeyEvent.VK_UP) && !pressed.contains(KeyEvent.VK_DOWN))||(!pressed.contains(KeyEvent.VK_UP) && pressed.contains(KeyEvent.VK_DOWN)))
+				if(((pressed.contains(KeyEvent.VK_LEFT) && !pressed.contains(KeyEvent.VK_RIGHT))||(!pressed.contains(KeyEvent.VK_LEFT) && pressed.contains(KeyEvent.VK_RIGHT)))&&((pressed.contains(KeyEvent.VK_UP) && !pressed.contains(KeyEvent.VK_DOWN))||(!pressed.contains(KeyEvent.VK_UP) && pressed.contains(KeyEvent.VK_DOWN))))
 					players[0].movspeed=players[0].SPEED/Math.sqrt(2);
-				else players[0].movspeed=players[0].SPEED
+				else players[0].movspeed=players[0].SPEED;
+				*/
             }
         }
     });
@@ -87,9 +138,9 @@ public class Main extends JPanel implements KeyListener {
                     players[1].ySpeed=players[1].movspeed;
                 } else players[1].ySpeed=0;
 				
-				if(((pressed.contains(KeyEvent.VK_LEFT) && !pressed.contains(KeyEvent.VK_RIGHT))||(!pressed.contains(KeyEvent.VK_LEFT) && pressed.contains(KeyEvent.VK_RIGHT)))&&((pressed.contains(KeyEvent.VK_UP) && !pressed.contains(KeyEvent.VK_DOWN))||(!pressed.contains(KeyEvent.VK_UP) && pressed.contains(KeyEvent.VK_DOWN)))
+				if(((pressed.contains(KeyEvent.VK_LEFT) && !pressed.contains(KeyEvent.VK_RIGHT))||(!pressed.contains(KeyEvent.VK_LEFT) && pressed.contains(KeyEvent.VK_RIGHT)))&&((pressed.contains(KeyEvent.VK_UP) && !pressed.contains(KeyEvent.VK_DOWN))||(!pressed.contains(KeyEvent.VK_UP) && pressed.contains(KeyEvent.VK_DOWN))))
 					players[1].movspeed=players[1].SPEED/Math.sqrt(2);
-				else players[1].movspeed=players[1].SPEED
+				else players[1].movspeed=players[1].SPEED;
             }
         }
     });
@@ -109,11 +160,14 @@ public class Main extends JPanel implements KeyListener {
             }
 
             // hit(PlayerXPos, PlayerYPos, PlayerXSpeed, PlayerYSpeed)
-            if(players[0].intersects(ball.getCenterX(), ball.getCenterY(), ball.SIZE, ball.SIZE))
+            if(players[0].intersects(ball.getCenterX(), ball.getCenterY(), ball.SIZE, ball.SIZE)){
                 ball.hit(players[0].getCenterX(), players[0].getCenterY(), players[0].xSpeed, players[0].ySpeed);
-            else if(players[1].intersects(ball.getCenterX(), ball.getCenterY(), ball.SIZE, ball.SIZE))
+				System.out.println("p0 hit");
+			}
+            else if(players[1].intersects(ball.getCenterX(), ball.getCenterY(), ball.SIZE, ball.SIZE)){
                 ball.hit(players[1].getCenterX(), players[1].getCenterY(), players[1].xSpeed, players[1].ySpeed);
-
+				System.out.println("p1 hit");
+			}
             frame.repaint();
         }
     }
