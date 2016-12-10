71a94b77c4949d43674ff35c8f4e347d839da9b4
robobok <robobok@users.noreply.github.com>
2016-12-10 11:30:57 +0200

    Update Ball.java

--- src/Ball.java
+++ src/Ball.java
@@ -9,34 +9,56 @@ class Ball extends Circle{
     double xSpeed = 0;
     double ySpeed = 0;
     final int SIZE=15;
-    double slow=0.05;
+    double slow=1.5;
+	double slowX=0;
+	double slowY=0;
     Thread ballMover=new Thread(new Runnable(){
         @Override
         public void run(){
             while(true){
+				
+				//Ball bounce
+                if((getCenterX()<=SIZE)&&xSpeed<0){
+                    xSpeed=-xSpeed+0.2;
+					System.out.println("bounce");
+				}
+				else if(xSpeed>0&&(getCenterX()>=Main.WIDTH-SIZE*2-20)){
+                    xSpeed=-xSpeed-0.2;
+					System.out.println("bounce");
+				}
+                if((getCenterY()<=SIZE)&&ySpeed<0){
+                    ySpeed=-ySpeed+0.2;
+					System.out.println("bounce");
+				}
+				else if(ySpeed>0&&(getCenterY()>=Main.HEIGHT-SIZE*2-20)){
+                    ySpeed=-ySpeed-0.2;
+					System.out.println("bounce");
+				}
+				
+				//Ball move
                 setCenterX(getCenterX() + xSpeed);
-                setCenterY(getCenterY() + ySpeed);
-                
-                //Ball bounce
-                if(getCenterX()<=SIZE||getCenterX()>=Main.WIDTH-SIZE)
-                    xSpeed=-xSpeed;
-                if(getCenterY()<=SIZE||getCenterY()>=Main.HEIGHT-SIZE)
-                    ySpeed=-ySpeed;
+                setCenterY(getCenterY() + ySpeed);               
                 
                 //Ball slows over time
-                if(xSpeed>slow)
-                    xSpeed -=slow;
-                else if(xSpeed<-slow)
-                    xSpeed +=slow;
+				double moveAngle;
+				moveAngle=Math.atan2(ySpeed,xSpeed);
+				slowX=slow*xSpeed/(xSpeed+ySpeed);
+				slowY=slow*ySpeed/(xSpeed+ySpeed);
+				
+				
+                if(xSpeed>=slowX)
+                    xSpeed -=slowX;
+                else if(xSpeed<=-slowX)
+                    xSpeed +=slowX;
                 else xSpeed=0;
-                if(ySpeed>slow)
-                    xSpeed -= slow;
-                else if(ySpeed<-slow)
-                    xSpeed += slow;
-                else xSpeed=0;
+                if(ySpeed>=slowY)
+                    ySpeed -= slowY;
+                else if(ySpeed<=-slowY)
+                    ySpeed += slowY;
+                else ySpeed=0;
                     
                 try{
-                    Thread.sleep(32);
+                    Thread.sleep(16);
                 }catch(InterruptedException e){
                     e.printStackTrace();
                 }
@@ -64,13 +86,13 @@ class Ball extends Circle{
     void hit(double xPos,double yPos,double xSpeed,double ySpeed){
         double ydist = yPos-this.yPos;
         double xdist = xPos-this.xPos;
-        this.xSpeed = xSpeed;
-        this.ySpeed = ySpeed;
+        this.xSpeed = xSpeed/2;
+        this.ySpeed = ySpeed/2;
         double angle = Math.atan2(ydist,xdist);
         double speed = Math.sqrt(ydist*ydist+xdist*xdist);
         this.xSpeed = -speed*Math.cos(angle);
-        this.ySpeed = -speed*Math.sin(angle);
-        this.xSpeed += xSpeed;	//Bu iki satırın tekrar eklenmesi top ve oyuncu arasındaki açının yanı sıra
-        this.ySpeed += ySpeed;	//topun oyuncunun hareket yönüne doğru da hareket etmesini sağlar
+		this.ySpeed = -speed*Math.sin(angle);
+        this.xSpeed += xSpeed/2;	//Bu iki satırın tekrar eklenmesi top ve oyuncu arasındaki açının yanı sıra
+        this.ySpeed += ySpeed/2;	//topun oyuncunun hareket yönüne doğru da hareket etmesini sağlar
     }
 }
